6ea12266f54ffb89107a4b39a8a9b8c9a37a059d
serg.bardeniuk <serg.bardeniuk@splynx.com>
2022-11-10 22:02:52 +0200

    lesson 20.1

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\ProductController;
+use App\Models\Product;
+use App\Http\Controllers\API\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +20,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/product', [\App\Http\Controllers\API\ProductController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/product', [ProductController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/auth/logout', [AuthController::class, 'logout']);
+
+Route::post('/token/create', [AuthController::class, 'createToken']);
+
+
+//class ProductTrasform
+//{
+//    public static function transform($products){
+//        $data = [];
+//        foreach ($products as $product) {
+//            $result = [
+//                'id' => $product->id,
+//                'title' => $product->title,
+//            ];
+//            if ($product->price < 50) {
+//                $data[] = $result;
+//            }
+//        }
+//        return $data;
+//    }
+//}
+//
+//
+//
+//Route::get('/product-transform', function () {
+//    return ProductTrasform::transform(Product::all());
+////    return \App\Models\Product::all();
+//});
